bb48758664675efc9a772a17c822e7e4f4527e9d
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-06-30 15:03:44 +0200

    updated css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,17 +4,17 @@ export default function Card(props) {
     return (
         <div className="card">
             <img src={imageUrl} className="card__img" alt={alt}/>
-            <div className="card-wrapper">
-                <div className="card__top">
-                    <i className="fa-solid fa-location-dot"></i>
-                    <p className="card__location">{location}</p>
-                    <a 
-                        href={googleMapsUrl}
-                        className="card__map-link" 
-                        target="_blank"
-                        rel="noreferrer">
-                        View on Google Maps</a>
-                </div>
+            <div className="card__top">
+                <i className="fa-solid fa-location-dot"></i>
+                <p className="card__location">{location}</p>
+                <a 
+                    href={googleMapsUrl}
+                    className="card__map-link" 
+                    target="_blank"
+                    rel="noreferrer">
+                    View on Google Maps</a>
+            </div>
+            <div className="card__bottom">
                 <h1 className="card__title">{title}</h1>
                 <p className="card__date">{startDate} - {endDate}</p>
                 <p className="card__description">{description}</p>
