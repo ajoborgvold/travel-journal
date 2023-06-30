b3d85021e1119dd36ddb11e1c6c3421fdcf9f359
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-06-30 09:43:12 +0200

    updated css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Card from './components/Card'
 import Header from './components/Header'
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
